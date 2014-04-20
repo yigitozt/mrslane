b155cc299f9dd0e8ed6a4380e2b6fd8736458ea7
yigitozturk <ygtozt@gmail.com>
2014-04-21 01:20:43 +0300

    no message

--- New Unity Project/Assets/Scripts/Level/PlayerSkill.cs
+++ New Unity Project/Assets/Scripts/Level/PlayerSkill.cs
@@ -2,14 +2,37 @@
 using System.Collections;
 
 public class PlayerSkill : MonoBehaviour {
+	public static bool bonusReceived;
+	public PhotonView myPhotonView;
+	public int selectedBonus;
+	public static float bonusTimer;
+	private float rollBonusTimer;
 
 	// Use this for initialization
 	void Start () {
-	
+		myPhotonView = gameObject.GetComponent<PhotonView>();
+		bonusTimer = 1.5f;
+		rollBonusTimer = 0.1f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(bonusReceived)
+		{
+			rollBonusTimer -= Time.deltaTime;
+			if(rollBonusTimer <= 0)
+			{
+				selectedBonus = Random.Range(1, 11);
+				rollBonusTimer = 0.1f;
+			}
+			
+			bonusTimer -= Time.deltaTime;
+			if(bonusTimer <= 0)
+			{
+				selectedBonus = Random.Range(1, 11);
+				bonusReceived = false;
+				Debug.Log(selectedBonus);
+			}
+		}
 	}
 }
--- New Unity Project/Assets/Scripts/Level/PlayerTriggerNetwork.cs
+++ New Unity Project/Assets/Scripts/Level/PlayerTriggerNetwork.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerTriggerNetwork : MonoBehaviour {
-
+public class PlayerTriggerNetwork : Photon.MonoBehaviour {
+	public PhotonView myPhotonView;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +11,29 @@ public class PlayerTriggerNetwork : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+		
+		if(stream.isWriting) 
+		{
+			//			stream.SendNext(transform.position);
+		}
+		else {
+			//			this.correctPlayerPos = (Vector3)stream.ReceiveNext();
+		}	
+	}
+
+	void OnTriggerEnter(Collider c)
+	{
+		if(c.gameObject.tag == "potion")
+		{
+			if(myPhotonView.isMine)
+			{
+				PlayerSkill.bonusTimer = 1.0f;
+				PlayerSkill.bonusReceived = true;
+				Debug.Log("TRIGGER WORK");
+			}
+		}
 	}
 }
